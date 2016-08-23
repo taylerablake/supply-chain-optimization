ce76d7172d6e1e17e8138f421e248834222ffe4c
Chris Holloman <chollo0042@bitbucket.org>
2016-08-23 13:03:20 +0000

    Coding to create full optimization across a range of values and explore the result.

--- reports - 1 - Planning/example-application.R
+++ reports - 1 - Planning/example-application.R
@@ -162,21 +162,30 @@ fc_expected_fulfillment <- ddply(data.frame(fc = fc_region_assignments,
 initial_allocation <- round(total_buy * fc_expected_fulfillment$expected_sales /
                               sum(fc_expected_fulfillment$expected_sales))
 
+initial_allocation <- total_buy * rep(.25, 4)
+
 #Set up cost parameters
-alpha_21 <- 1 #fixed cost of delivering a unit
-alpha_22 <- 3 #variable cost of delivering a unit 1 distance unit
-min_transfer <- 10 #Minimum number of units you can ship between FCs
-alpha_11 <- 4
-alpha_12 <- .1
-alpha_13 <- 1
+alpha_21 <- 5 #fixed cost of delivering a unit
+alpha_22 <- 5 #variable cost of delivering a unit 1 distance unit
+min_transfer <- 25 #Minimum number of units you can ship between FCs
+alpha_11 <- 4 #fixed cost of doing a transfer
+alpha_12 <- 1 #per unit cost of doing a transfer
+alpha_13 <- 5 #variable cost of doing a transfer 1 distance unit
 AUR <- 29.99 #Item sale price
-delta <- 0.0069 #Discount factor per additional unit over demand (markdown rate)
-                #0.0069 implies that AUR is cut in half for an over-order of 100 units
+delta <- 0.00069 #Discount factor per additional unit over demand (markdown rate)
+                #0.00069 implies that AUR is cut in half for an over-order of 1000 units
 
 #Simulate the system
 options(warn = 2)
 n_generations <- 100 #This must be less than or equal to n_samples
-total_value <- rep(NA, n_generations) #Store the value
+value_storage <- data.frame(total_value = rep(NA, n_generations),
+                            amt_sold = rep(NA, n_generations),
+                            total_buy = rep(NA, n_generations),
+                            total_demand = rep(NA, n_generations),
+                            total_delivery_cost = rep(NA, n_generations),
+                            total_transfer_cost = rep(NA, n_generations),
+                            full_price = rep(NA, n_generations),
+                            marked_down = rep(NA, n_generations))
 for (this_generation in 1:n_generations) {
   print(this_generation)
   
@@ -344,22 +353,321 @@ for (this_generation in 1:n_generations) {
   
   #Record the total value of the strategy
   amt_sold <- sum(total_fulfilled)
-  total_value[this_generation] <- amt_sold * AUR * (total_buy < sum(true_buys)) +
+  total_demand <- sum(true_buys)
+  total_value <- amt_sold * AUR * (total_buy < total_demand) +
     ((amt_sold * AUR + (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold))) * 
-       (sum(initial_allocation) >= sum(true_buys))) -
+       (total_buy >= total_demand)) -
     total_delivery_cost -
     total_transfer_cost
-  
+  value_storage$total_value[this_generation] <- total_value
+  value_storage$amt_sold[this_generation] <- amt_sold
+  value_storage$total_buy[this_generation] <- total_buy
+  value_storage$total_demand[this_generation] <- total_demand
+  value_storage$total_delivery_cost[this_generation] <- total_delivery_cost
+  value_storage$total_transfer_cost[this_generation] <- total_transfer_cost
+  value_storage$full_price[this_generation] <- amt_sold * AUR
+  value_storage$marked_down[this_generation] <- (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold)) * (total_buy >= total_demand)
+
 }
 
+expected_value <- mean(value_storage$total_value)
+value_storage
 
-ggplot(household_locations) +
-  aes(x = x, y = y, color = as.factor(region)) +
-  geom_point() +
-  geom_text(data = fulfill_centers_locations,
-            aes(x = x, y = y, label = fc),
-            color = "#000000", size = 4) +
-  geom_point(data = household_locations[order_hhs,],
-             aes(x = x, y = y),
-             color = "#000000", size = 3)
+opt_result <- value_storage
 
+plot(sort(opt_result$total_value),
+     sort(value_storage$total_value))
+
+#Implement an optimization on this.
+set.seed(1978)
+#Set up cost parameters
+alpha_21 <- 5 #fixed cost of delivering a unit
+alpha_22 <- 5 #variable cost of delivering a unit 1 distance unit
+min_transfer <- 25 #Minimum number of units you can ship between FCs
+alpha_11 <- 4 #fixed cost of doing a transfer
+alpha_12 <- 1 #per unit cost of doing a transfer
+alpha_13 <- 5 #variable cost of doing a transfer 1 distance unit
+AUR <- 29.99 #Item sale price
+delta <- 0.00069 #Discount factor per additional unit over demand (markdown rate)
+#0.00069 implies that AUR is cut in half for an over-order of 1000 units
+
+fc_expected_fulfillment <- ddply(data.frame(fc = fc_region_assignments,
+                                            expected_sales = expected_sales),
+                                 .(fc), summarize,
+                                 expected_sales = sum(expected_sales))
+initial_allocation <- round(total_buy * fc_expected_fulfillment$expected_sales /
+                              sum(fc_expected_fulfillment$expected_sales))
+
+
+#Make a matrix of the allocation schemes to try
+allocations <- matrix(initial_allocation, nrow = 1)
+allocation_list <- vector("list", n_fulfill_centers - 1)
+for (j in 1:(n_fulfill_centers - 1)) {
+  allocation_list[[j]] <- round(initial_allocation[j], -2)
+  allocation_list[[j]] <- seq(max(allocation_list[[j]] - 200, 100),
+                              min(allocation_list[[j]] + 200, 100 * floor(total_buy / 100)),
+                              by = 100)
+}
+other_allocations <- as.matrix(expand.grid(allocation_list))
+other_allocations <- cbind(other_allocations,
+                           total_buy - apply(other_allocations, 1, sum))
+allocations <- rbind(allocations, other_allocations)
+
+options(warn = 2)
+output_storage <- NULL
+t1 <- Sys.time()
+for (this_allocation in 1:nrow(allocations)) {
+  print(this_allocation)
+  
+  initial_allocation <- as.numeric(allocations[this_allocation, ])
+
+  n_generations <- 100 #This must be less than or equal to n_samples
+  value_storage <- data.frame(total_value = rep(NA, n_generations),
+                              amt_sold = rep(NA, n_generations),
+                              total_buy = rep(NA, n_generations),
+                              total_demand = rep(NA, n_generations),
+                              total_delivery_cost = rep(NA, n_generations),
+                              total_transfer_cost = rep(NA, n_generations),
+                              full_price = rep(NA, n_generations),
+                              marked_down = rep(NA, n_generations))
+  
+  for (this_generation in 1:n_generations) {
+
+    #Generate the number of buys in each region across all time points
+    expected_buys <- matrix(NA, n_regions, n_time_periods)
+    true_buys <- matrix(NA, n_regions, n_time_periods)
+    for (j in 1:n_regions) {
+      this_index <- n_samples * (j - 1) + this_generation
+      expected_buys[j, ] <- posterior_samples$theta_1[this_index] * 
+        exp(-(1:n_time_periods) / posterior_samples$theta_2[this_index])
+      true_buys[j, ] <- rpois(n_time_periods, expected_buys[j, ])
+    }
+    
+    #Go over time applying the fulfillment and transfer rules.  Also, calculate
+    #total costs as you go.
+    current_inventory <- initial_allocation
+    total_fulfilled <- 0
+    total_delivery_cost <- 0
+    total_transfer_cost <- 0
+    for (time_point in 1:n_time_periods) {
+      
+      #If we're all out of inventory, we're done
+      if (sum(current_inventory) == 0)
+        next
+      
+      #Randomly select the set of houses that placed orders
+      true_total_buy <- sum(true_buys[, time_point])
+      order_hhs <- NULL
+      for (j in 1:n_regions) {
+        order_hhs <- c(order_hhs, sample(which(household_locations$region == j),
+                                         true_buys[j, time_point], replace = FALSE))
+      }
+      
+      #Calculate distances from each ordering HH to each FC
+      deliver_distances <- as.matrix(dist(rbind(fulfill_centers_locations[, c("x", "y")],
+                                                household_locations[order_hhs, c("x", "y")])))
+      deliver_distances <- deliver_distances[1:n_fulfill_centers, 
+                                             -(1:n_fulfill_centers)]
+      deliver_distances <- matrix(as.numeric(deliver_distances), nrow = n_fulfill_centers)
+      
+      #Find the closest FC to each HH
+      closest_delivery <- apply(deliver_distances, 2, which.min)
+      
+      #Count up how many need to be sent from each FC
+      n_to_fulfill <- as.data.frame(table(closest_delivery))
+      n_to_fulfill$closest_delivery <- as.character(n_to_fulfill$closest_delivery)
+      n_to_fulfill <- merge(n_to_fulfill, 
+                            data.frame(closest_delivery = as.character(1:n_fulfill_centers)),
+                            all = TRUE)
+      n_to_fulfill$Freq[is.na(n_to_fulfill$Freq)] <- 0
+      n_to_fulfill <- n_to_fulfill[order(n_to_fulfill$closest_delivery),]
+      
+      #Make a table of current delivery strategy.
+      current_delivery_strategy <- matrix(0, n_fulfill_centers, true_total_buy)
+      current_delivery_strategy[matrix(c(closest_delivery, 1:true_total_buy),
+                                       ncol = 2)] <- 1
+      
+      #Check if they all have enough.
+      if (min(current_inventory - n_to_fulfill$Freq) < 0) { #Not enough to fulfill
+        
+        #Check if we're going to run completely out of inventory.
+        if (sum(current_inventory) < sum(n_to_fulfill$Freq)) {  #We're going to run out of inventory at this time point
+          
+          #Randomly select which ones we'll actually fulfill
+          random_delivery <- sample(1:true_total_buy, sum(current_inventory))
+          
+          #Update the objects
+          true_total_buy <- sum(current_inventory)
+          order_hhs <- order_hhs[random_delivery]
+          deliver_distances <- deliver_distances[, random_delivery]
+          deliver_distances <- matrix(as.numeric(deliver_distances), nrow = n_fulfill_centers)
+          closest_delivery <- apply(deliver_distances, 2, which.min)
+          n_to_fulfill <- as.data.frame(table(closest_delivery))
+          n_to_fulfill$closest_delivery <- as.character(n_to_fulfill$closest_delivery)
+          n_to_fulfill <- merge(n_to_fulfill, 
+                                data.frame(closest_delivery = as.character(1:n_fulfill_centers)),
+                                all = TRUE)
+          n_to_fulfill$Freq[is.na(n_to_fulfill$Freq)] <- 0
+          n_to_fulfill <- n_to_fulfill[order(n_to_fulfill$closest_delivery),]
+          
+          #Make a table of current delivery strategy.
+          current_delivery_strategy <- matrix(0, n_fulfill_centers, true_total_buy)
+          current_delivery_strategy[matrix(c(closest_delivery, 1:true_total_buy),
+                                           ncol = 2)] <- 1
+        }
+        
+        #Modify this to a viable delivery strategy
+        fulfill_total <- apply(current_delivery_strategy, 1, sum)
+        while(min(current_inventory - fulfill_total) < 0) {
+          worst_fc <- which.min(current_inventory - fulfill_total)[1]
+          move_to <- which(current_inventory > fulfill_total)
+          to_move <- which.min(deliver_distances[move_to, current_delivery_strategy[worst_fc,] == 1])[1]
+          move_to_index <- to_move %% length(move_to)
+          if (move_to_index == 0) {
+            move_to_index <- length(move_to)
+          }
+          fc <- move_to[move_to_index]
+          hh <- which(current_delivery_strategy[worst_fc,] == 1)[ceiling(to_move / length(move_to))]
+          current_delivery_strategy[fc, hh]  <- 1
+          current_delivery_strategy[worst_fc, hh] <- 0
+          fulfill_total <- apply(current_delivery_strategy, 1, sum)
+        }
+        
+        #Optimize the fulfillment strategy
+        #TODO!!!
+        
+      }
+      
+      #Calculate the cost of fulfilling under this strategy
+      delivery_cost <- sum(alpha_21 + alpha_22 * 
+                             as.numeric(deliver_distances)[as.numeric(current_delivery_strategy) == 1])
+      
+      #Update inventory
+      fulfill_total <- apply(current_delivery_strategy, 1, sum)
+      current_inventory <- current_inventory - fulfill_total
+      
+      #Determine whether we're going to do any transfers among FCs
+      if (sum(current_inventory) > 0 & time_point < n_time_periods) {
+        
+        #Calculate expected buys remaining in each FC
+        buys_remaining <- apply(matrix(expected_buys[, (time_point + 1):n_time_periods],
+                                       nrow = n_regions), 1, sum)
+        fc_expected_fulfillment <- ddply(data.frame(fc = fc_region_assignments,
+                                                    buys_remaining = buys_remaining),
+                                         .(fc), summarize,
+                                         buys_remaining = sum(buys_remaining))
+        
+        #Only do something if we have a differential of at least min_transfer in each
+        #direction
+        gap <- current_inventory - fc_expected_fulfillment$buys_remaining
+        
+        transfer_cost <- 0
+        while (max(gap) > min_transfer & -min(gap) > min_transfer) {
+          
+          #Identify which ones you can transfer from and to
+          from_fcs <- which(gap > min_transfer)
+          to_fcs <- which(gap < -min_transfer)
+          
+          #Which one has the most to give?
+          giver <- from_fcs[which.max(gap[from_fcs])]
+          
+          #Which one needs the most?
+          receiver <- to_fcs[which.min(gap[to_fcs])]
+          
+          #Transfer the minimum of the amount needed or the amount available to give
+          to_transfer <- round(min(-gap[receiver], gap[giver]))
+          current_inventory[giver] <- current_inventory[giver] - to_transfer
+          current_inventory[receiver] <- current_inventory[receiver] + to_transfer
+          
+          transfer_cost <- transfer_cost + alpha_11 + alpha_12 * to_transfer +
+            alpha_13 * transfer_distances[giver, receiver]
+          
+          gap <- current_inventory - fc_expected_fulfillment$buys_remaining
+          
+        }
+        
+      }
+      
+      total_fulfilled <- total_fulfilled + fulfill_total
+      total_delivery_cost <- total_delivery_cost + delivery_cost
+      total_transfer_cost <- total_transfer_cost + transfer_cost
+      
+      
+    }
+    
+    #Record the total value of the strategy
+    amt_sold <- sum(total_fulfilled)
+    total_demand <- sum(true_buys)
+    total_value <- amt_sold * AUR * (total_buy < total_demand) +
+      ((amt_sold * AUR + (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold))) * 
+         (total_buy >= total_demand)) -
+      total_delivery_cost -
+      total_transfer_cost
+    value_storage$total_value[this_generation] <- total_value
+    value_storage$amt_sold[this_generation] <- amt_sold
+    value_storage$total_buy[this_generation] <- total_buy
+    value_storage$total_demand[this_generation] <- total_demand
+    value_storage$total_delivery_cost[this_generation] <- total_delivery_cost
+    value_storage$total_transfer_cost[this_generation] <- total_transfer_cost
+    value_storage$full_price[this_generation] <- amt_sold * AUR
+    value_storage$marked_down[this_generation] <- (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold)) * (total_buy >= total_demand)
+    
+  }
+  
+  value_storage$allocation_number <- this_allocation
+  output_storage <- rbind(output_storage, value_storage)
+  t2 <- Sys.time()
+  print(t2 - t1)
+}
+
+save.image("data/example-application-run-20160822.RData")
+
+allocation_frame <- as.data.frame(allocations)
+allocation_frame$allocation_number <- 1:nrow(allocation_frame)
+output_storage <- merge(output_storage, allocation_frame)
+
+fit <- lm(total_value ~ Var1 + Var2 + Var3
+          + Var1 * Var2 + Var1 * Var3 + Var2 * Var3
+          + I(Var1^2) + I(Var2^2) + I(Var3^2),
+          data = output_storage)
+
+pred_frame <- output_storage
+pred_frame$predicted <- predict(fit)
+pred_frame <- unique(pred_frame[, c("Var1", "Var2", "Var3", "V4", "predicted")])
+pred_frame[which.max(pred_frame$predicted),]
+
+pred_frame <- expand.grid(Var1 = seq(100, 500, by = 10),
+                          Var2 = seq(300, 700, by = 10),
+                          Var3 = seq(500, 900, by = 10))
+pred_frame$predicted <- predict(fit, newdata = pred_frame)
+p <- ggplot(pred_frame[pred_frame$Var3 == 700,]) +
+  aes(x = Var1, y = Var2, color = predicted) +
+  geom_point(size = 5) +
+  facet_wrap(~ Var3) +
+  ggtitle("X") +
+  scale_color_gradientn(colors = terrain.colors(10))
+p <- ggplot(pred_frame) +
+  aes(x = Var1, y = Var2, z = predicted) +
+  geom_contour() +
+  facet_wrap(~ Var3) +
+  ggtitle("X")
+p <- ggplot(pred_frame) +
+  aes(x = Var1, y = Var2, z = predicted) +
+  geom_raster(aes(fill = predicted)) +
+  scale_fill_gradient(low = "#0000FF",
+                         high = "#FF0000") +
+  geom_contour(color = "#000000") +
+  facet_wrap(~ Var3) +
+  ggtitle("X")
+p
+
+avgs <- ddply(output_storage, .(Var1, Var2, Var3, V4),
+              summarize, ev = mean(total_value))
+avgs[which.max(avgs$ev),]
+
+p <- ggplot(avgs[avgs$Var1 != 343,]) +
+  aes(x = Var1, y = Var2, color = ev) +
+  geom_point(size = 10) +
+  facet_wrap(~ Var3) +
+  ggtitle("X")
+aap_format(p, color_scheme = "blue", display_metadata = FALSE)
