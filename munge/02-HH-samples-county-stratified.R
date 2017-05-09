3ec529493743aca9782f5715ec901b7055f040ea
Blake <tblake@icct.com>
2017-05-09 10:20:09 -0400

    add script to sample HHs stratified by county to avoid ugly chunky map

---
+++ munge/02-HH-samples-county-stratified.R
@@ -0,0 +1,35 @@
+
+library(maps)
+library(maptools)
+library(sp)
+library(rgdal)
+library(ggmap)
+
+source(file.path(getwd(),"lib","latlon2state.R"))
+
+pop_by_county <- read.csv(file.path(getwd(),
+                                   "data",
+                                   "census",
+                                   myYear,
+                                   "co-est2016-alldata.csv"),
+                         header = TRUE)
+pop_by_county <- pop_by_county[,c("SUMLEV",
+                                  "STATE",
+                                  "STNAME",
+                                  "COUNTY",
+                                  "CTYNAME",
+                                  "POPESTIMATE2016")] %>%
+  transform(.,STATENAME=str_to_lower(STNAME))
+
+
+counties <- map('county', fill=TRUE, col="transparent", plot=FALSE)
+county_df <- counties$names %>%
+  str_split(.,",") %>%
+  ldply(.,function(c) {data.frame(state=c[1],county=c[2])}) %>%
+  ddply(.,.(state),transform,
+        COUNTY=1:length(county))
+table(county_df$state)
+sum(pop_by_county$STNAME=="California")
+
+
+
