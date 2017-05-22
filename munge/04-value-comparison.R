66acfd75d8967c7efcc4282bdd80ed9e11941697
taylerb <tblake@icct.com>
2017-05-22 20:33:18 +0000

    use posterior modes as true parameter values, calculate value for each best initial allocation

---
+++ munge/04-value-comparison.R
@@ -0,0 +1,313 @@
+
+
+load(file.path(getwd(),"data",
+               "example-application-run-2017-05-16_18-35-13.Rdata"))
+
+avgs <- ddply(output_storage, .(Var1, Var2, Var3, Var4, V5),
+              summarize, ev = mean(total_value))
+naive_avgs <- ddply(naive_output_storage, .(Var1, Var2, Var3, Var4, V5),
+                    summarize, ev = mean(total_value))
+
+naive_avgs[which.max(naive_avgs$ev),]
+
+avgs[which.max(avgs$ev),]
+avgs[which.max(naive_avgs$ev),]
+
+
+
+
+
+## for the smart way of determining expected value of an allocation
+## determine the value of the "best" allocation when the sales are
+## simulated taking the mode of the posterior distribution to be
+## the "true" parameter value.
+
+## theta_1 ~ n(10,2) -> theta_1=10
+## theta_2 ~ gamma(3*100/3,3) -> theta_2=100/3
+##mu_kt = 10*exp(-3*t/100)
+
+
+
+storage <- data.frame(total_value = rep(NA, 2),
+                            amt_sold = rep(NA, 2),
+                            total_buy = rep(NA, 2),
+                            total_demand = rep(NA, 2),
+                            total_delivery_cost = rep(NA, 2),
+                            #total_transfer_cost = rep(NA, 2),
+                            full_price = rep(NA, 2),
+                            marked_down = rep(NA, 2))
+
+expected_buys <- matrix(NA, n_regions, n_time_periods)
+true_buys <- matrix(NA, n_regions, n_time_periods)
+for (j in 1:n_regions) {
+  this_index <- n_samples * (j - 1) + 1
+  expected_buys[j, ] <- 10 * exp(-((1:n_time_periods)*3 )/ 100 )
+  true_buys[j, ] <- rpois(n_time_periods, expected_buys[j, ])
+}
+
+
+
+#Go over time applying the fulfillment and transfer rules.  Also, calculate
+#total costs as you go.
+
+
+initial_allocation <- avgs[which.max(avgs$ev),1:5]
+current_inventory <- initial_allocation
+total_fulfilled <- 0
+total_delivery_cost <- 0
+
+for (time_point in 1:n_time_periods) {
+  
+  #If we're all out of inventory, we're done
+  if (sum(current_inventory) == 0)
+    next
+  
+  #Randomly select the set of houses that placed orders
+  true_total_buy <- sum(true_buys[, time_point])
+  order_hhs <- NULL
+  for (j in 1:n_regions) {
+    order_hhs <- c(order_hhs, sample(which(HH_samples$region == j),
+                                     true_buys[j, time_point], replace = FALSE))
+  }
+  
+  
+  #Calculate distances from each ordering HH to each FC
+  deliver_distances <- as.matrix(dist(rbind(fulfill_centers_locations[, c("lon", "lat")],
+                                            HH_samples[order_hhs, c("lon", "lat")])))
+  deliver_distances <- deliver_distances[1:n_fulfill_centers, 
+                                         -(1:n_fulfill_centers)]
+  deliver_distances <- matrix(as.numeric(deliver_distances), nrow = n_fulfill_centers)
+  
+  #Find the closest FC to each HH
+  closest_delivery <- apply(deliver_distances, 2, which.min)
+  
+  #Count up how many need to be sent from each FC
+  n_to_fulfill <- as.data.frame(table(closest_delivery))
+  n_to_fulfill$closest_delivery <- as.character(n_to_fulfill$closest_delivery)
+  n_to_fulfill <- merge(n_to_fulfill, 
+                        data.frame(closest_delivery = as.character(1:n_fulfill_centers)),
+                        all = TRUE)
+  n_to_fulfill$Freq[is.na(n_to_fulfill$Freq)] <- 0
+  n_to_fulfill <- n_to_fulfill[order(n_to_fulfill$closest_delivery),]
+  
+  #Make a table of current delivery strategy.
+  current_delivery_strategy <- matrix(0, n_fulfill_centers, true_total_buy)
+  current_delivery_strategy[matrix(c(closest_delivery, 1:true_total_buy),
+                                   ncol = 2)] <- 1
+  
+  #Check if they all have enough.
+  if (min(current_inventory - n_to_fulfill$Freq) < 0) { #Not enough to fulfill
+    
+    #Check if we're going to run completely out of inventory.
+    if (sum(current_inventory) < sum(n_to_fulfill$Freq)) {  #We're going to run out of inventory at this time point
+      
+      #Randomly select which ones we'll actually fulfill
+      random_delivery <- sample(1:true_total_buy, sum(current_inventory))
+      
+      #Update the objects
+      true_total_buy <- sum(current_inventory)
+      order_hhs <- order_hhs[random_delivery]
+      deliver_distances <- deliver_distances[, random_delivery]
+      deliver_distances <- matrix(as.numeric(deliver_distances), nrow = n_fulfill_centers)
+      closest_delivery <- apply(deliver_distances, 2, which.min)
+      n_to_fulfill <- as.data.frame(table(closest_delivery))
+      n_to_fulfill$closest_delivery <- as.character(n_to_fulfill$closest_delivery)
+      n_to_fulfill <- merge(n_to_fulfill, 
+                            data.frame(closest_delivery = as.character(1:n_fulfill_centers)),
+                            all = TRUE)
+      n_to_fulfill$Freq[is.na(n_to_fulfill$Freq)] <- 0
+      n_to_fulfill <- n_to_fulfill[order(n_to_fulfill$closest_delivery),]
+      
+      #Make a table of current delivery strategy.
+      current_delivery_strategy <- matrix(0, n_fulfill_centers, true_total_buy)
+      current_delivery_strategy[matrix(c(closest_delivery, 1:true_total_buy),
+                                       ncol = 2)] <- 1
+    }
+    
+    #Modify this to a viable delivery strategy
+    fulfill_total <- apply(current_delivery_strategy, 1, sum)
+    while(min(current_inventory - fulfill_total) < 0) {
+      worst_fc <- which.min(current_inventory - fulfill_total)[1]
+      move_to <- which(current_inventory > fulfill_total)
+      to_move <- which.min(deliver_distances[move_to, current_delivery_strategy[worst_fc,] == 1])[1]
+      move_to_index <- to_move %% length(move_to)
+      if (move_to_index == 0) {
+        move_to_index <- length(move_to)
+      }
+      fc <- move_to[move_to_index]
+      hh <- which(current_delivery_strategy[worst_fc,] == 1)[ceiling(to_move / length(move_to))]
+      current_delivery_strategy[fc, hh]  <- 1
+      current_delivery_strategy[worst_fc, hh] <- 0
+      fulfill_total <- apply(current_delivery_strategy, 1, sum)
+    }
+    
+    #Optimize the fulfillment strategy
+    #TODO!!!
+    
+  }
+  
+  #Calculate the cost of fulfilling under this strategy
+  delivery_cost <- sum(alpha_1 + alpha_2 * 
+                         as.numeric(deliver_distances)[as.numeric(current_delivery_strategy) == 1] +
+                         alpha_3 *
+                         floor(10 * as.numeric(deliver_distances)[as.numeric(current_delivery_strategy) == 1]))
+  
+  #Update inventory
+  fulfill_total <- apply(current_delivery_strategy, 1, sum)
+  current_inventory <- current_inventory - fulfill_total
+  
+  total_fulfilled <- total_fulfilled + fulfill_total
+  total_delivery_cost <- total_delivery_cost + delivery_cost
+  
+}
+
+amt_sold <- sum(total_fulfilled)
+total_demand <- sum(true_buys)
+total_value <- amt_sold * AUR * (total_buy < total_demand) +
+  ((amt_sold * AUR + (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold))) * 
+     (total_buy >= total_demand)) -
+  total_delivery_cost 
+
+storage$total_value[1] <- total_value
+storage$amt_sold[1] <- amt_sold
+storage$total_buy[1] <- total_buy
+storage$total_demand[1] <- total_demand
+storage$total_delivery_cost[1] <- total_delivery_cost
+#storage$total_transfer_cost[this_generation] <- total_transfer_cost
+storage$full_price[1] <- amt_sold * AUR
+storage$marked_down[1] <- (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold)) * (total_buy >= total_demand)
+
+
+
+initial_allocation <- naive_avgs[which.max(naive_avgs$ev),1:5]
+current_inventory <- initial_allocation
+total_fulfilled <- 0
+total_delivery_cost <- 0
+
+for (time_point in 1:n_time_periods) {
+  
+  #If we're all out of inventory, we're done
+  if (sum(current_inventory) == 0)
+    next
+  
+  #Randomly select the set of houses that placed orders
+  true_total_buy <- sum(true_buys[, time_point])
+  order_hhs <- NULL
+  for (j in 1:n_regions) {
+    order_hhs <- c(order_hhs, sample(which(HH_samples$region == j),
+                                     true_buys[j, time_point], replace = FALSE))
+  }
+  
+  
+  #Calculate distances from each ordering HH to each FC
+  deliver_distances <- as.matrix(dist(rbind(fulfill_centers_locations[, c("lon", "lat")],
+                                            HH_samples[order_hhs, c("lon", "lat")])))
+  deliver_distances <- deliver_distances[1:n_fulfill_centers, 
+                                         -(1:n_fulfill_centers)]
+  deliver_distances <- matrix(as.numeric(deliver_distances), nrow = n_fulfill_centers)
+  
+  #Find the closest FC to each HH
+  closest_delivery <- apply(deliver_distances, 2, which.min)
+  
+  #Count up how many need to be sent from each FC
+  n_to_fulfill <- as.data.frame(table(closest_delivery))
+  n_to_fulfill$closest_delivery <- as.character(n_to_fulfill$closest_delivery)
+  n_to_fulfill <- merge(n_to_fulfill, 
+                        data.frame(closest_delivery = as.character(1:n_fulfill_centers)),
+                        all = TRUE)
+  n_to_fulfill$Freq[is.na(n_to_fulfill$Freq)] <- 0
+  n_to_fulfill <- n_to_fulfill[order(n_to_fulfill$closest_delivery),]
+  
+  #Make a table of current delivery strategy.
+  current_delivery_strategy <- matrix(0, n_fulfill_centers, true_total_buy)
+  current_delivery_strategy[matrix(c(closest_delivery, 1:true_total_buy),
+                                   ncol = 2)] <- 1
+  
+  #Check if they all have enough.
+  if (min(current_inventory - n_to_fulfill$Freq) < 0) { #Not enough to fulfill
+    
+    #Check if we're going to run completely out of inventory.
+    if (sum(current_inventory) < sum(n_to_fulfill$Freq)) {  #We're going to run out of inventory at this time point
+      
+      #Randomly select which ones we'll actually fulfill
+      random_delivery <- sample(1:true_total_buy, sum(current_inventory))
+      
+      #Update the objects
+      true_total_buy <- sum(current_inventory)
+      order_hhs <- order_hhs[random_delivery]
+      deliver_distances <- deliver_distances[, random_delivery]
+      deliver_distances <- matrix(as.numeric(deliver_distances), nrow = n_fulfill_centers)
+      closest_delivery <- apply(deliver_distances, 2, which.min)
+      n_to_fulfill <- as.data.frame(table(closest_delivery))
+      n_to_fulfill$closest_delivery <- as.character(n_to_fulfill$closest_delivery)
+      n_to_fulfill <- merge(n_to_fulfill, 
+                            data.frame(closest_delivery = as.character(1:n_fulfill_centers)),
+                            all = TRUE)
+      n_to_fulfill$Freq[is.na(n_to_fulfill$Freq)] <- 0
+      n_to_fulfill <- n_to_fulfill[order(n_to_fulfill$closest_delivery),]
+      
+      #Make a table of current delivery strategy.
+      current_delivery_strategy <- matrix(0, n_fulfill_centers, true_total_buy)
+      current_delivery_strategy[matrix(c(closest_delivery, 1:true_total_buy),
+                                       ncol = 2)] <- 1
+    }
+    
+    #Modify this to a viable delivery strategy
+    fulfill_total <- apply(current_delivery_strategy, 1, sum)
+    while(min(current_inventory - fulfill_total) < 0) {
+      worst_fc <- which.min(current_inventory - fulfill_total)[1]
+      move_to <- which(current_inventory > fulfill_total)
+      to_move <- which.min(deliver_distances[move_to, current_delivery_strategy[worst_fc,] == 1])[1]
+      move_to_index <- to_move %% length(move_to)
+      if (move_to_index == 0) {
+        move_to_index <- length(move_to)
+      }
+      fc <- move_to[move_to_index]
+      hh <- which(current_delivery_strategy[worst_fc,] == 1)[ceiling(to_move / length(move_to))]
+      current_delivery_strategy[fc, hh]  <- 1
+      current_delivery_strategy[worst_fc, hh] <- 0
+      fulfill_total <- apply(current_delivery_strategy, 1, sum)
+    }
+    
+    #Optimize the fulfillment strategy
+    #TODO!!!
+    
+  }
+  
+  #Calculate the cost of fulfilling under this strategy
+  delivery_cost <- sum(alpha_1 + alpha_2 * 
+                         as.numeric(deliver_distances)[as.numeric(current_delivery_strategy) == 1] +
+                         alpha_3 *
+                         floor(10 * as.numeric(deliver_distances)[as.numeric(current_delivery_strategy) == 1]))
+  
+  #Update inventory
+  fulfill_total <- apply(current_delivery_strategy, 1, sum)
+  current_inventory <- current_inventory - fulfill_total
+  
+  total_fulfilled <- total_fulfilled + fulfill_total
+  total_delivery_cost <- total_delivery_cost + delivery_cost
+  
+}
+
+amt_sold <- sum(total_fulfilled)
+total_demand <- sum(true_buys)
+total_value <- amt_sold * AUR * (total_buy < total_demand) +
+  ((amt_sold * AUR + (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold))) * 
+     (total_buy >= total_demand)) -
+  total_delivery_cost 
+
+storage$total_value[2] <- total_value
+storage$amt_sold[2] <- amt_sold
+storage$total_buy[2] <- total_buy
+storage$total_demand[2] <- total_demand
+storage$total_delivery_cost[2] <- total_delivery_cost
+#storage$total_transfer_cost[this_generation] <- total_transfer_cost
+storage$full_price[2] <- amt_sold * AUR
+storage$marked_down[2] <- (total_buy - amt_sold) * AUR * exp(-delta * (total_buy - amt_sold)) * (total_buy >= total_demand)
+
+
+data.frame(storage,data.frame(rbind(avgs[which.max(avgs$ev),1:5],
+                 naive_avgs[which.max(naive_avgs$ev),1:5])))
+
+
+
