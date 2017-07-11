5bff4628972f3e310e5bd7e542a7dfc60fcd8e0d
Blake <tblake@icct.com>
2017-07-11 14:33:35 -0400

    add fulfillment center territories to household region map

--- munge/01-HH-samples-fulfillment-centers-locations.R
+++ munge/01-HH-samples-fulfillment-centers-locations.R
@@ -123,10 +123,12 @@ household_locations$state <- str_to_title(gsub("(.*):.*", "\\1", household_locat
 
 
 region <- rep(NA, nrow(household_locations))
-region[household_locations$state=="North Dakota"] <- "East Region"
 region[household_locations$x < -94.3] <- "West Region"
 region[household_locations$x >= -94.3] <- "East Region"
-region[household_locations$state %in% c("Missouri","Iowa","Arkansas","Louisiana","Minnesota")] <- "East Region"
+region[household_locations$state=="North Dakota"] <- "East Region"
+region[household_locations$state=="Louisiana"] <- "West Region"
+region[household_locations$state=="Texas"] <- "West Region"
+region[household_locations$state %in% c("Missouri","Iowa","Arkansas","Minnesota")] <- "East Region"
 household_locations$region <- region
 
 #Create the fulfillment centers
@@ -174,25 +176,81 @@ ggplot() +
   #guides(colour=guide_legend("Region",override.aes = list(alpha = 1,size=2))) +
   guides(colour="none") +
   scale_color_tableau() 
+ggsave("doc/img/two-region-household-map.png",
+       height = 5.5, width = 9.17)
 
 
 
-map <- get_map('United States',
-               zoom=4,
-               color="bw",
-               maptype='hybrid')
-ggmap(map,
-      darken=0.2) +
+
+
+
+
+
+
+
+#Find the closest fulfillment center to each household
+fc_to_hh_distances <- matrix(NA, nrow(fulfill_centers_locations),
+                             nrow(household_locations))
+for (i in 1:nrow(fulfill_centers_locations)) {
+  fc_to_hh_distances[i, ] <- haversine_distance(fulfill_centers_locations$y[i],
+                                                fulfill_centers_locations$x[i],
+                                                household_locations$y,
+                                                household_locations$x)
+}
+fc_to_hh_distance <- apply(fc_to_hh_distances, 2, which.min)
+
+alpha_shapes <- vector("list", n_fulfill_centers)
+for (i in 1:n_fulfill_centers) {
+  alpha_shapes[[i]] <- ahull(household_locations$x[fc_to_hh_distance == i],
+                             household_locations$y[fc_to_hh_distance == i],
+                             .8)
+}
+alpha_polys <- NULL
+for (i in 1:n_fulfill_centers)
+  alpha_polys <- rbind(alpha_polys,
+                       data.frame(fc = i,
+                                  data.frame(alpha_shapes[[i]]$arcs)[, c("c1", "c2")]))
+alpha_polys$fc <- as.factor(alpha_polys$fc)
+ggplot() +
+  geom_polygon(data = map.usa_country,
+               aes(x = long, y = lat, group = group),
+               fill = "#484848") +
   geom_point(data=household_locations[sample(1:nrow(household_locations),size=30000),],
              aes(x=x,
                  y=y,
-                 colour=factor(region))) +
+                 colour=factor(region)),
+             alpha=0.7,
+             size=0.6) +
+  coord_map(projection = "albers", lat0 = 30, lat1 = 40, xlim = c(-121,-73), ylim = c(25,51)) +
+  #labs(title = "Fulfillment center & order locations") +
+  theme(panel.background = element_rect(fill = "#292929")
+        ,plot.background = element_rect(fill = "#292929")
+        ,legend.background = element_rect(fill = "#292929")
+        ,legend.key = element_blank()
+        ,legend.text=element_text(color="#A1A1A1", size=12)
+        ,legend.title=element_text(color="#A1A1A1", size=16)
+        ,panel.grid = element_blank()
+        ,axis.title = element_blank()
+        ,axis.text = element_blank()
+        ,axis.ticks = element_blank()
+        ,text = element_text(color = "#A1A1A1")
+        ,plot.title = element_text(size = 20)) +
+  geom_point(data=fulfill_centers_locations,
+             aes(x,y),
+             color = "white",
+             size = 8) +
   geom_text(data=fulfill_centers_locations,
-            aes(x=lon,y=lat,label = fc),
-            color = "white", size = 6) +
-  guides(colour=guide_legend("Region")) +
-  scale_colour_tableau('tableau10')
-rm(grid_points)
+            aes(x,y,label=fc),
+            color = "black",
+            size = 4) +
+  #guides(colour=guide_legend("Region",override.aes = list(alpha = 1,size=2))) +
+  guides(colour="none") +
+  scale_color_tableau() +
+  geom_polygon(data = alpha_polys,
+               aes(x = c1, y = c2, group=fc, fill="white"),
+               colour="white",
+               alpha=0.001)
+
 
 
 cache("household_locations")
